d88e3a76d20816908346d2a855a0a9d3624f3bd2
CVSCharan <charan.cvs@gmail.com>
2022-08-07 16:13:21 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a className="navbar-brand" href="!#">
+          <img
+            src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/food-munch-logo-img.png"
+            className="img1"
+            alt="img1"
+          />
         </a>
-      </header>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse ml-auto"
+          id="navbarNavAltMarkup"
+        >
+          <div className="navbar-nav ml-lg-auto">
+            <a className="nav-link active" href="!#">
+              Home <span className="sr-only">(current)</span>
+            </a>
+            <a className="nav-link text-white" href="!#">
+              Why choose Us?
+            </a>
+            <a className="nav-link text-white" href="!#">
+              Explore our Menu
+            </a>
+            <a className="nav-link text-white" href="!#">
+              Delivery & Payment
+            </a>
+            <a className="nav-link text-white" href="!#">
+              Follow Us
+            </a>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 }
